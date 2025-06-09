0ff45106193d48f2f7c6fbd02775d49aa8ee89af
yongilkim01 <yongil.k01@gmail.com>
2025-06-09 23:20:51 +0900

    feat: implement kratos animation blueprint

---
+++ Ragnarok/Source/Ragnarok/Public/Animation/AnimInstances/KratosAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstances/CharacterAnimInstance.h"
+#include "KratosAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAGNAROK_API UKratosAnimInstance : public UCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Ragnarok/Source/Ragnarok/Public/Animation/AnimInstances/RagnarokAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "RagnarokAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAGNAROK_API URagnarokAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
