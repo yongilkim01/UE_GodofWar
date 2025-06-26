dd97745e848f53d9b11e6a54bef03f85e15fd391
yongilkim01 <yongil.k01@gmail.com>
2025-06-26 18:55:02 +0900

    feat: implement kratos equip weapon gameplay ability

---
+++ Ragnarok/Source/Ragnarok/Public/RagnarokContent/Characters/Kratos/GameplayAbilities/KratosEquipWeaponGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RagnarokContent/Characters/Kratos/GameplayAbilities/KratosGameplayAbility.h"
+#include "KratosEquipWeaponGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAGNAROK_API UKratosEquipWeaponGameplayAbility : public UKratosGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
