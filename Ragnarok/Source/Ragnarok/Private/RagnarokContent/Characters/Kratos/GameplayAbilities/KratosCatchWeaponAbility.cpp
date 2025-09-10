56d24d94ba2fc71a5745f350cac0bde7841f4021
yongilkim01 <yongil.k01@gmail.com>
2025-09-10 21:41:24 +0900

    feat: implement weapon catch system foundation with event-driven gameplay ability pattern (WIP)
    무기 캐치 시스템 기초 구현 (개발 중):
      - KratosCatchWeaponAbility 클래스 추가 및 기초 함수 구현
      - 캐치 애니메이션 재생, 카메라 흔들림, 무기 부착 함수 생성
      - GA_KratosCatchWeapon 게임플레이 어빌리티 블루프린트 에셋 추가
      - DA_KratosStartUp 데이터 에셋에 GA_KratosCatchWeapon 추가
    
    Weapon Catch System Foundation:
      - Add KratosCatchWeaponAbility class with basic function implementation
      - Create catch animation playback, camera shake, and weapon attachment functions
      - Add GA_KratosCatchWeapon gameplay ability blueprint asset
      - Add GA_KratosCatchWeapon to DA_KratosStartUp data asset

--- Ragnarok/Source/Ragnarok/Private/RagnarokContent/Characters/Kratos/GameplayAbilities/KratosCatchWeaponAbility.cpp
+++ Ragnarok/Source/Ragnarok/Private/RagnarokContent/Characters/Kratos/GameplayAbilities/KratosCatchWeaponAbility.cpp
@@ -3,6 +3,64 @@
 
 #include "RagnarokContent/Characters/Kratos/GameplayAbilities/KratosCatchWeaponAbility.h"
 
+#include "RagnarokEngine/Kismet/Debug/RagnarokDebugHelper.h"
+
 UKratosCatchWeaponAbility::UKratosCatchWeaponAbility()
 {
+	bShowDebug = true;
+
+}
+
+void UKratosCatchWeaponAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::ActivateAbility"));
+
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+
+}
+
+void UKratosCatchWeaponAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::EndAbility"));
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+
+}
+
+bool UKratosCatchWeaponAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::CanActivateAbility"));
+
+	if (false == Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UKratosCatchWeaponAbility::PlayCatchAnimation()
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::PlayCatchAnimation"));
+
+}
+
+void UKratosCatchWeaponAbility::TriggerCameraShake()
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::TriggerCameraShake"));
+
+}
+
+void UKratosCatchWeaponAbility::AttachWeaponToKratos()
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::AttachWeaponToKratos"));
+
+}
+
+void UKratosCatchWeaponAbility::OnCatchAnimationComplete()
+{
+	if (true == bShowDebug) Debug::Print(TEXT("UKratosCatchWeaponAbility::OnCatchAnimationComplete"));
+
 }
--- Ragnarok/Source/Ragnarok/Public/RagnarokContent/Characters/Kratos/GameplayAbilities/KratosCatchWeaponAbility.h
+++ Ragnarok/Source/Ragnarok/Public/RagnarokContent/Characters/Kratos/GameplayAbilities/KratosCatchWeaponAbility.h
@@ -6,6 +6,9 @@
 #include "RagnarokContent/Characters/Kratos/GameplayAbilities/KratosGameplayAbility.h"
 #include "KratosCatchWeaponAbility.generated.h"
 
+class UAbilityTask_PlayMontageAndWait;
+class AKratosWeapon;
+
 /**
  * 
  */
@@ -18,6 +21,46 @@ public:
 	UKratosCatchWeaponAbility();
 
 protected:
+	//~ Begin UGameplayAbility Interface.
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
 
-	
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	virtual bool CanActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags = nullptr,
+		const FGameplayTagContainer* TargetTags = nullptr,
+		OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	//~ End UGameplayAbility Interface.
+
+private:
+	void PlayCatchAnimation();
+	void TriggerCameraShake();
+	void AttachWeaponToKratos();
+
+	UFUNCTION()
+	void OnCatchAnimationComplete();
+
+private:
+	UPROPERTY(EditAnywhere)
+	UAnimMontage* CatchAnimMontage = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UCameraShakeBase> CatchCameraShake = nullptr;
+
+	UPROPERTY()
+	UAbilityTask_PlayMontageAndWait* PlayMontageTask = nullptr;
+
+	UPROPERTY()
+	AKratosWeapon* CatchingWeapon = nullptr;
+
 };
