c500f95ddb5886e1d32ce0d2875b1d648fe7487f
yongilkim01 <yongil.k01@gmail.com>
2025-09-13 23:40:48 +0900

    style: add comments to weapon class header files
    무기 클래스 헤더 파일 주석 추가:
      - RagnarokWeapon.h 주석 추가
      - KratosWeapon.h 주석 추가
      - LeviathanAxe.h 주석 추가
    
    Add comments to weapon class header files:
      - Add comments to RagnarokWeapon.h
      - Add comments to KratosWeapon.h
      - Add comments to LeviathanAxe.h

--- Ragnarok/Source/Ragnarok/Public/RagnarokContent/Characters/Kratos/KratosWeapon.h
+++ Ragnarok/Source/Ragnarok/Public/RagnarokContent/Characters/Kratos/KratosWeapon.h
@@ -13,7 +13,11 @@ class UProjectileMovementComponent;
 class UTimelineComponent;
 
 /**
+ * 크레토스 무기 베이스 클래스
+ * 크레토스가 사용할 수 있는 모든 무기의 기본이 되는 클래스로 게임플레이 어빌리티와 투사체 기능을 제공한다
  * 
+ * Kratos weapon base class.
+ * Base class for all weapons that Kratos can use, providing gameplay ability and projectile functionality.
  */
 UCLASS()
 class RAGNAROK_API AKratosWeapon : public ARagnarokWeapon
@@ -21,49 +25,108 @@ class RAGNAROK_API AKratosWeapon : public ARagnarokWeapon
 	GENERATED_BODY()
 
 public:
+	/** 생성자 */
 	AKratosWeapon();
 
+	/** 무기를 초기화하는 함수 */
 	virtual void InitWeapon() override;
 
+	/**
+	 * 부여받은 어빌리티 스펙 핸들을 할당하는 함수
+	 *
+	 * @param InSpecHandleArray 할당할 어빌리티 스펙 핸들 배열
+	 */
 	void AssignGratnAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandleArray);
+	
+	/**
+	 * 무기의 소유자를 설정하는 함수
+	 *
+	 * @param RagnarokCharacter 무기를 소유할 라그나로크 캐릭터
+	 */
 	virtual void SetOwner(ARagnarokCharacter* RagnarokCharacter) override;
+	
+	/** 부여받은 어빌리티 스펙 핸들 배열을 반환하는 함수 */
 	TArray<FGameplayAbilitySpecHandle>& GetGrantedAbilitySpecHandleArray();
 	
+	/**
+	 * 무기를 던지는 함수 (가상 함수 - 하위 클래스에서 구현)
+	 *
+	 * @param CameraRotation 카메라 회전값
+	 * @param CameraLocation 카메라 위치
+	 * @param CameraForwardVector 카메라 전방 벡터
+	 */
 	virtual void ThrowWeapon(FRotator CameraRotation, FVector CameraLocation, FVector CameraForwardVector) {}
+	
+	/** 무기를 회수하는 함수 (가상 함수 - 하위 클래스에서 구현) */
 	virtual void RecallWeapon() {}
+	
+	/** 무기를 정지시키는 함수 (가상 함수 - 하위 클래스에서 구현) */
 	virtual void StopWeapon() {}
+	
+	/** 무기 트레일을 시작하는 함수 (가상 함수 - 하위 클래스에서 구현) */
 	virtual void StartWeaponTrail() {}
+	
+	/** 무기 트레일을 종료하는 함수 (가상 함수 - 하위 클래스에서 구현) */
 	virtual void EndWeaponTrail() {}
 
+	/**
+	 * 무기를 장착 해제하는 함수
+	 *
+	 * @param OwnerActor 무기를 소유한 액터
+	 * @param SocketName 무기가 부착된 소켓 이름
+	 */
 	void UnUequipWeapon(const AActor* OwnerActor, FName SocketName);
 
 protected:
 	//~ Begin AActor Interface.
+	
+	/** 게임 시작 시 호출됨 */
 	virtual void BeginPlay() override;
+	
+	/** 매 프레임 호출됨 */
 	virtual void Tick(float DeltaTime) override;
 	//~ End AActor Interface.
 
 	//~ Begin ARagnarokWeapon Interface.
+	
+	/** 무기 데이터 에셋을 로드하는 함수 */
 	virtual void LoadWeaponDataAsset() override;
+	
+	/**
+	 * 무기 프라이머리 데이터 에셋을 로드하는 함수
+	 *
+	 * @param PDAAssetObject 로드할 프라이머리 데이터 에셋 오브젝트
+	 */
 	virtual void LoadWeaponPrimaryDataAsset(UObject* PDAAssetObject) override;
 	//~ End ARagnarokWeapon Interface.
 
 private:
+	/**
+	 * 액터로부터 애니메이션 클래스 레이어를 연결 해제하는 함수
+	 *
+	 * @param UnlinkActor 연결 해제할 액터
+	 */
 	void UnlinkAnimClassLayersFromActor(const AActor* UnlinkActor);
 
 private:
+	/** 부여받은 어빌리티 스펙 핸들 배열 */
 	TArray<FGameplayAbilitySpecHandle> GrantedAbilitySpecHandleArray;
 
 protected:
-	UPROPERTY(EditAnywhere)
+	/** 투사체 움직임 컴포넌트 */
+	UPROPERTY(EditAnywhere, Category = "KratosWeapon|Components", meta = (DisplayName = "Projectile Movement Component"))
 	UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
 
+	/** 크레토스 무기 프라이머리 데이터 에셋 */
+	UPROPERTY(EditAnywhere, Category = "KratosWeapon|Data", meta = (DisplayName = "KratosWeapon Primary Data Aaset"))
 	UItemPrimaryAssetKratosWeapon* KratosWeaponPDA = nullptr;
 
+	/** 무기를 소유한 크레토스 캐릭터에 대한 약한 참조 */
 	UPROPERTY()
 	TWeakObjectPtr<AKratos> OwnerKratos;
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	/** 무기 데이터 구조체 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KratosWeapon|Data", meta = (DisplayName = "Weapon Data"))
 	FKratosWeaponData WeaponData;
 };
--- Ragnarok/Source/Ragnarok/Public/RagnarokEngine/Objects/Items/Weapons/RagnarokWeapon.h
+++ Ragnarok/Source/Ragnarok/Public/RagnarokEngine/Objects/Items/Weapons/RagnarokWeapon.h
@@ -14,32 +14,93 @@ class ARagnarokCharacter;
 
 struct FAttachmentTransformRules;
 
+/** 무기 초기화 완료 시 호출되는 델리게이트 */
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWeaponInitialized);
+
+/** 타겟과 상호작용 시 호출되는 델리게이트 */
 DECLARE_DELEGATE_OneParam(FOnTargetInteractedDelegate, AActor*);
 
+/**
+ * 라그나로크 무기 베이스 클래스
+ * 게임 내 모든 무기의 기본이 되는 클래스로 메시, 충돌, 초기화 등의 기본 기능을 제공한다
+ * 
+ * Ragnarok weapon base class.
+ * Base class for all weapons in the game, providing basic functionality like mesh, collision, and initialization.
+ */
 UCLASS()
 class RAGNAROK_API ARagnarokWeapon : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
+	/** 생성자 */
 	ARagnarokWeapon();
 
-public:
-	virtual void InitWeapon() {}
-	virtual void SetOwner(ARagnarokCharacter* RagnarokCharacter) {};
-
-	void AttachWeaponToActor(const AActor* ActorToAttach, const FAttachmentTransformRules& AttachmentRules, FName SocketName);
-
-protected:
 	//~ Begin AActor Interface.
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	//~ End AActor Interface.
 
+	/** 무기를 초기화하는 함수 (가상 함수 - 하위 클래스에서 구현) */
+	virtual void InitWeapon() {}
+	
+	/**
+	 * 무기의 소유자를 설정하는 함수 (가상 함수 - 하위 클래스에서 구현)
+	 *
+	 * @param RagnarokCharacter 무기를 소유할 라그나로크 캐릭터
+	 */
+	virtual void SetOwner(ARagnarokCharacter* RagnarokCharacter) {};
+
+	/**
+	 * 무기를 액터에 부착하는 함수
+	 *
+	 * @param ActorToAttach 무기를 부착할 액터
+	 * @param AttachmentRules 부착 트랜스폼 규칙
+	 * @param SocketName 부착할 소켓 이름
+	 */
+	void AttachWeaponToActor(const AActor* ActorToAttach, const FAttachmentTransformRules& AttachmentRules, FName SocketName);
+
+	// Getters (Inline Functions)
+	/** 무기 메시를 반환하는 함수 */
+	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh() const { return WeaponMesh; }
+	/** 무기 충돌 컴포넌트를 반환하는 함수 */
+	FORCEINLINE UBoxComponent* GetWeaponCollision() const { return WeaponCollision; }
+	/** 무기 초기화 여부를 반환하는 함수 */
+	FORCEINLINE bool IsInitialize() const { return bInitialized; }
+	/** 현재 무기 상태를 반환하는 함수 */
+	FORCEINLINE ERagnarokWeaponState GetWeaponState() const { return CurWeaponState; }
+
+	// Public Variables (BlueprintAssignable Delegates)
+	/** 무기 초기화 완료 시 브로드캐스트되는 델리게이트 */
+	UPROPERTY(BlueprintAssignable, Category = "RagnarokWeapon|Events", meta = (DisplayName = "On Weapon Initialized"))
+	FOnWeaponInitialized OnWeaponInitialized;
+
+	/** 무기가 타겟에 충돌했을 때 호출되는 델리게이트 */
+	FOnTargetInteractedDelegate OnWeaponHitTarget;
+	/** 무기가 타겟으로부터 분리될 때 호출되는 델리게이트 */
+	FOnTargetInteractedDelegate OnWeaponPulledFromTarget;
+
+protected:
+	/** 무기 데이터 에셋을 로드하는 함수 (가상 함수 - 하위 클래스에서 구현) */
 	virtual void LoadWeaponDataAsset();
+	
+	/**
+	 * 무기 프라이머리 데이터 에셋을 로드하는 함수 (가상 함수 - 하위 클래스에서 구현)
+	 *
+	 * @param PDAAssetObject 로드할 프라이머리 데이터 에셋 오브젝트
+	 */
 	virtual void LoadWeaponPrimaryDataAsset(UObject* PDAAssetObject);
 
+	/**
+	 * 충돌 박스 오버랩 시작 시 호출되는 함수
+	 *
+	 * @param OverlappedComponent 오버랩된 컴포넌트
+	 * @param OtherActor 충돌한 다른 액터
+	 * @param OtherComp 충돌한 다른 컴포넌트
+	 * @param OtherBodyIndex 다른 바디 인덱스
+	 * @param bFromSweep 스윕으로부터 발생한 충돌인지 여부
+	 * @param SweepResult 스윕 결과
+	 */
 	UFUNCTION()
 	virtual void OnCollisionBoxBeginOverlap(
 		UPrimitiveComponent*OverlappedComponent,
@@ -49,6 +110,14 @@ protected:
 		bool bFromSweep,
 		const FHitResult& SweepResult);
 
+	/**
+	 * 충돌 박스 오버랩 종료 시 호출되는 함수
+	 *
+	 * @param OverlappedComponent 오버랩된 컴포넌트
+	 * @param OtherActor 충돌한 다른 액터
+	 * @param OtherComp 충돌한 다른 컴포넌트
+	 * @param OtherBodyIndex 다른 바디 인덱스
+	 */
 	UFUNCTION()
 	virtual void OnCollisionBoxEndOverlap(
 		UPrimitiveComponent* OverlappedComponent,
@@ -56,35 +125,30 @@ protected:
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
 
-protected:
-	UPROPERTY(EditAnywhere)
+	// Protected Variables (UPROPERTY for Editor/Blueprint access)
+	/** 루트 씬 컴포넌트 */
+	UPROPERTY(EditAnywhere, Category = "RagnarokWeapon|Components", meta = (DisplayName = "Root Scene Component"))
 	USceneComponent* RootSceneComponent = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UItemPrimaryAssetKratosWeapon* WeaponPDA = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FPrimaryAssetId WeaponPDAId;
-
-protected:
-	UPROPERTY(EditAnywhere)
+	/** 무기 스켈레탈 메시 컴포넌트 */
+	UPROPERTY(EditAnywhere, Category = "RagnarokWeapon|Components", meta = (DisplayName = "Weapon Mesh"))
 	USkeletalMeshComponent* WeaponMesh = nullptr;
 
-	UPROPERTY(EditAnywhere)
+	/** 무기 충돌 박스 컴포넌트 */
+	UPROPERTY(EditAnywhere, Category = "RagnarokWeapon|Components", meta = (DisplayName = "Weapon Collision"))
 	UBoxComponent* WeaponCollision = nullptr;
 
+	/** 무기 프라이머리 데이터 에셋 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RagnarokWeapon|Data", meta = (DisplayName = "Weapon Primary Data Asset"))
+	UItemPrimaryAssetKratosWeapon* WeaponPDA = nullptr;
+
+	/** 무기 프라이머리 데이터 에셋 ID */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RagnarokWeapon|Data", meta = (DisplayName = "Weapon PDA ID"))
+	FPrimaryAssetId WeaponPDAId;
+
+	/** 무기 초기화 완료 여부 */
 	bool bInitialized = false;
+	
+	/** 현재 무기 상태 */
 	ERagnarokWeaponState CurWeaponState = ERagnarokWeaponState::ERWS_None;
-
-public:
-	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh() const { return WeaponMesh; }
-	FORCEINLINE UBoxComponent* GetWeaponCollision() const { return WeaponCollision; }
-	FORCEINLINE bool IsInitialize() const { return bInitialized; }
-	FORCEINLINE ERagnarokWeaponState GetWeaponState() const { return CurWeaponState; }
-
-	UPROPERTY(BlueprintAssignable)
-	FOnWeaponInitialized OnWeaponInitialized;
-
-	FOnTargetInteractedDelegate OnWeaponHitTarget;
-	FOnTargetInteractedDelegate OnWeaponPulledFromTarget;
 };
